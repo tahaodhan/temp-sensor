41969a3d62b5e2f99ecf21453241df145094eefb
Taha Odhan <tahaodhan@github.com>
2025-04-16 13:18:52 -0230

    added the graph part

--- temp-sensor-wasm/temp-run/build.sh
+++ temp-sensor-wasm/temp-run/build.sh
@@ -1,25 +1,26 @@
-# Copyright (C) 2019 Intel Corporation.  All rights reserved.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#!/bin/bash
 
-WAMR_DIR=${PWD}/../../..
+# Set these based on your actual folder locations
+WASI_SDK_PATH="$HOME/wasi-sdk"
+WAMR_DIR="$HOME/wasm-micro-runtime"
 
 echo "Build wasm app .."
 $WASI_SDK_PATH/bin/clang -O3 \
-        -z stack-size=4096 -Wl,--initial-memory=65536 \
-        -o test.wasm main.c \
-        -Wl,--export=main -Wl,--export=__main_argc_argv \
-        -Wl,--export=__data_end -Wl,--export=__heap_base \
-        -Wl,--strip-all,--no-entry \
-        -Wl,--allow-undefined \
-        -nostdlib \
+    -z stack-size=4096 -Wl,--initial-memory=65536 \
+    -o temp-run.wasm temp-sense.c \
+    -Wl,--export=main -Wl,--export=__main_argc_argv \
+    -Wl,--export=__data_end -Wl,--export=__heap_base \
+    -Wl,--strip-all,--no-entry \
+    -Wl,--allow-undefined \
+    -nostdlib
 
 echo "Build binarydump tool .."
 rm -fr build && mkdir build && cd build
-cmake $WAMR_PATH/test-tools/binarydump-tool
+cmake $WAMR_DIR/test-tools/binarydump-tool
 make
 cd ..
 
 echo "Generate test_wasm.h .."
-./build/binarydump -o ../main/test_wasm.h -n wasm_test_file_interp test.wasm
+./build/binarydump -o ../main/test_wasm.h -n wasm_test_file_interp temp-run.wasm
 
 echo "Done"
